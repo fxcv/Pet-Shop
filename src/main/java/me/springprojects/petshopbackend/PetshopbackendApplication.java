7d379f22258c26ccafb10596c37554fb6f616963
fxcv <fabiana7x@wp.pl>
2023-02-18 12:34:06 +0100

    Created project

---
+++ src/main/java/me/springprojects/petshopbackend/PetshopbackendApplication.java
@@ -0,0 +1,13 @@
+package me.springprojects.petshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/springprojects/petshopbackend/PetshopbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.springprojects.petshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
